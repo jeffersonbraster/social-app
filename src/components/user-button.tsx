fec91362f0c833f9ac8529ecd5da7f62d08ec630
jefferson.brandao <jefferson.brandao@g.globo>
2024-07-31 19:32:12 -0300

    configurando dark theme

--- src/components/user-button.tsx
+++ src/components/user-button.tsx
@@ -6,14 +6,19 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import UserAvatar from "./user-avatar";
 import Link from "next/link";
-import { LogOutIcon, UserIcon } from "lucide-react";
+import { Check, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
 import { logout } from "@/app/(auth)/actions";
 import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
 
 interface UserButtonProps {
   className?: string;
@@ -21,6 +26,7 @@ interface UserButtonProps {
 
 const UserButton = ({ className }: UserButtonProps) => {
   const { user } = useSession();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -41,6 +47,50 @@ const UserButton = ({ className }: UserButtonProps) => {
             Perfil
           </DropdownMenuItem>
         </Link>
+
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Monitor className="mr-2 size-4" />
+            Tema
+          </DropdownMenuSubTrigger>
+
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem
+                onClick={() => setTheme("system")}
+                className="cursor-pointer"
+              >
+                <Monitor className="mr-2 size-4" />
+                Sistema
+                {theme === "system" && (
+                  <Check className="ms-2 size-4 text-green-400" />
+                )}
+              </DropdownMenuItem>
+
+              <DropdownMenuItem
+                onClick={() => setTheme("light")}
+                className="cursor-pointer"
+              >
+                <Sun className="mr-2 size-4" />
+                light
+                {theme === "light" && (
+                  <Check className="ms-2 size-4 text-green-400" />
+                )}
+              </DropdownMenuItem>
+
+              <DropdownMenuItem
+                onClick={() => setTheme("dark")}
+                className="cursor-pointer"
+              >
+                <Moon className="mr-2 size-4" />
+                Dark
+                {theme === "dark" && (
+                  <Check className="ms-2 size-4 text-green-400" />
+                )}
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
 
         <DropdownMenuSeparator />
 
